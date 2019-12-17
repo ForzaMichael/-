77348304cbe36ce72a81eb6daba8c0e2f7935760
e <liqi_zb@cmos.chinamobile.com>
2019-12-17 09:37:32 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,193 @@
 <template>
-  <div class="main">
-    <div class="nav-list" ref="listItem" @mouseleave="mouseLeaveHandle">
-      <div
-        v-for="(nav,index) in navs"
-        :key="index"
-        class="nav-item"
-        :class="{active:itemActive===index}"
-        @mouseenter="setCurrentNav(index)"
-        @click="setActiveNav(index)"
-      >{{nav.name}}</div>
-      <div class="nav-underline" :style="navUnderlineStyle"></div>
+  <div class="wrap">
+    <div class="score">得分：0</div>
+    <div class="canvasWrap" ref="canvasArea">
+      <canvas id="canvas"></canvas>
     </div>
+    <button class="startGameBtn" @click="startRain">startGame</button>
   </div>
 </template>
-
 <script>
+const redPacket = {
+  x: "x轴位置",
+  y: "y轴位置",
+  radius: "红包大小",
+  img: "缓存好的红包图片",
+  speed: "红包的下落速度"
+};
 export default {
-  name: "articallist",
   data() {
     return {
-      currentNav: "",
-      currentNavStyle: {},
-      itemActive: "",
-      activeNavStyle: {},
-      navs: [
-        { name: "1" },
-        { name: "Gardelli" },
-        { name: "V60" },
-        { name: "Geisha" },
-        { name: "Visual Studio Code" }
-      ]
+      innerHeight: "",
+      innerWidth: "",
+      timer: null,
+      imgArr: [
+        {
+          img:
+            "http://pic.90sjimg.com/original_origin_pic/18/09/18/8fe63a446fb8766a5cce14ebbbbe4952.png!/fwfh/804x1201/quality/90/unsharp/true/compress/true/watermark/url/LzkwX3dhdGVyX3Y2LnBuZw==/repeat/true"
+        },
+        {
+          img:
+            "https://hbimg.huabanimg.com/04ab13e4b937fc871370ac49662cc562902600de5eab5-XIxPDN_fw658"
+        },
+        {
+          img:
+            "http://img.yipic.cn/thumb/ff6d64dc/a0a4a5ac/e6ee916c/c9480c96/effect_big_ff6d64dca0a4a5ace6ee916cc9480c96.jpg?x-oss-process=image/format,webp/sharpen,100"
+        },
+        {
+          img:
+            "http://img.yipic.cn/thumb/5200ceba/f292f2e2/e03bb2a8/340f3687/effect_small_5200cebaf292f2e2e03bb2a8340f3687.png?x-oss-process=image/format,webp/sharpen,100"
+        }
+      ],
+      ctx: "",
+      redPacketArr: []
     };
   },
-  methods: {
-    setCurrentNav(index) {
-      this.currentNav = index;
-      this.currentNavStyle = this.getNavStyle(index);
-    },
-    getNavStyle(index) {
-      let childNodes = this.$refs.listItem.childNodes;
-      let width = childNodes[index].clientWidth;
-      let left = 0;
-      if (index > 0) {
-        for (let i = 0; i < index; i++) {
-          left += childNodes[i].clientWidth;
-        }
-      }
-      return { width, left };
-    },
-    setActiveNav(index) {
-      this.itemActive = index;
-      this.activeNavStyle = this.getNavStyle(index);
-    },
-    mouseLeaveHandle() {
-      this.currentNavStyle = null;
-    }
-  },
-  computed: {
-    navUnderlineStyle() {
-      let style = this.currentNavStyle || this.activeNavStyle;
-      return {
-        width: `${style.width}px`,
-        left: `${style.left}px`
+  mounted() {
+    this.initCanvas();
+    this.innerHeight = this.$refs.canvasArea.offsetHeight;
+    this.innerWidth = this.$refs.canvasArea.offsetWidth;
+    this.$nextTick().then(() => {
+      let that = this;
+      window.onresize = function() {
+        window.clearTimeout(this.timer);
+        this.timer = window.setTimeout(() => {
+          that.resizeCanvas();
+        }, 100);
       };
+    });
+  },
+  methods: {
+    startRain() {
+      this.pushRedPackets(); // 不断增加红包
+      this.moveRedPackets(); // 红包开始运动
+      // 开始10秒倒计时
+      this.runCountdownTimer = setInterval(() => {
+        //倒计时15s后，停止动画
+        window.clearTimeout(this.addredPacketsTimer);
+        window.cancelAnimationFrame(this.moveRedPacketAnimation);
+      }, 15000);
+    },
+    initCanvas() {
+      const canvas = document.getElementById("canvas");
+      if (canvas.getContext) {
+        this.ctx = canvas.getContext("2d");
+        this.loadImgs(this.imgArr);
+      }
+    },
+    resizeCanvas() {
+      this.innerHeight = this.$refs.canvasArea.offsetHeight;
+      this.innerWidth = this.$refs.canvasArea.offsetWidth;
+      document.getElementById("canvas").setAttribute("width", this.innerWidth);
+      document
+        .getElementById("canvas")
+        .setAttribute("height", this.innerHeight);
+    },
+    loadImgs(arr) {
+      return new Promise(resolve => {
+        let count = 0;
+        // 循环图片数组，每张图片都生成一个新的图片对象
+        const len = arr.length;
+        for (let i = 0; i < len; i++) {
+          // 创建图片对象
+          const image = new Image();
+          // 成功的异步回调
+          image.onload = () => {
+            count++;
+            arr.splice(i, 1, {
+              img: image
+            });
+            // 这里说明 整个图片数组arr里面的图片全都加载好了
+            if (count == len) {
+              this.preloaded = true;
+              resolve();
+            }
+          };
+          image.src = arr[i].img;
+        }
+      });
+    },
+    //生成红包
+    pushRedPackets() {
+      // 每次随机生成1~3个红包
+      const random = this.randomRound(1, 3);
+      let arr = [];
+      for (let i = 0; i < random; i++) {
+        // 创建新的红包对象
+        const newRedPacket = {
+          x: this.randomRound(5, this.innerWidth - 5), // 横向随机  红包不要贴近边边
+          y: -Math.random() * 150, // -150内高度 随机
+          radius: this.randomRound(
+            this.innerWidth * 0.05,
+            this.innerWidth * 0.1
+          ), // 红包宽度
+          img: this.imgArr[this.randomRound(0, this.imgArr.length - 1)].img, // 随机取一个红包图片对象
+          speed: this.randomRound(
+            this.innerHeight * 0.01,
+            this.innerHeight * 0.02
+          )
+        };
+        // console.log(newRedPacket);
+        arr.push(newRedPacket);
+      }
+      // 每次都插入一批新红包对象arr到运动的红包数组this.redPacketArr
+      this.redPacketArr = [...this.redPacketArr, ...arr];
+      // 间隔多久生成一批红包
+      this.addredPacketsTimer = setTimeout(() => {
+        this.pushRedPackets();
+      }, 200);
+    },
+    //清空绘制canvas,更新红包位置
+    moveRedPackets() {
+      // 清空canvas
+      this.ctx.clearRect(0, 0, this.innerWidth, this.innerHeight);
+      // 绘制新的一帧动画
+      this.drawRedPackets();
+      // 不断执行绘制，形成动画
+      this.moveRedPacketAnimation = window.requestAnimationFrame(
+        this.moveRedPackets
+      );
+    },
+    //canvas红包对象绘制
+    drawRedPackets() {
+      // 遍历这个红包对象数组，绘制某个红包对象时，更新原来的红包对象
+      this.redPacketArr.forEach((redPacket, index) => {
+        const newRedPacket = {
+          x: redPacket.x,
+          // 每次改变y值
+          y: redPacket.y + redPacket.speed,
+          radius: redPacket.radius,
+          img: redPacket.img,
+          speed: redPacket.speed
+        };
+        // 替换掉原来的它
+        this.redPacketArr.splice(index, 1, newRedPacket);
+        this.ctx.drawImage(
+          redPacket.img,
+          redPacket.x,
+          redPacket.y,
+          redPacket.radius,
+          redPacket.radius * 1.5
+        );
+      });
+    },
+    randomRound(start, end) {
+      return Math.round(Math.random() * (end - start) + start);
     }
   }
 };
 </script>
 <style scoped>
-.main {
-  font-family: "Segoe UI";
+.wrap {
+  width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
 }
-.main .nav-list {
-  position: relative;
-  height: 36px;
+.canvasWrap {
+  width: 100%;
+  height: 100%;
 }
-.main .nav-list .nav-item {
-  font-size: 16px;
-  list-style: none;
-  padding: 10px;
-  margin: 0;
-  float: left;
-  text-align: center;
-  cursor: pointer;
-}
-
-.main .nav-list .nav-underline {
-  position: absolute;
-  bottom: 0;
-  display: block;
-  height: 2px;
-  background-color: red;
-  transition: all 0.2s ease-out;
-}
-.active {
-  color: red;
+#canvas {
+  width: 100%;
+  height: 500px;
 }
 </style>
